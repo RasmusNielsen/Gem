12e43a279c9627884199ac1efd5123b1d0065671
Kasper Kronborg <kasper@kronborg.info>
2021-06-01 23:30:46 +0200

    Added onboarding videos

--- ToDoApp/Views/Onboarding/ObItemView.swift
+++ ToDoApp/Views/Onboarding/ObItemView.swift
@@ -6,44 +6,48 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ObItemView: View {
-  
-  let description: String
-  
-  var body: some View {
-    VStack(spacing: 0) {
-      HStack {
-        Spacer()
-        
-        Circle()
-          .fill(Color.white)
-          .frame(width: 15, height: 15)
-
-        Circle()
-          .fill(Color.white)
-          .frame(width: 15, height: 15)
-
-        Circle()
-          .fill(Color.white)
-          .frame(width: 15, height: 15)
-        
-        Spacer()
-      }
-      .frame(height: 90)
-      .background(Color.gray)
-      
-      Rectangle().fill(Color.gray).frame(width: 50, height: 50)
-      
-      Text(self.description).foregroundColor(Theme.Palette.Text.Primary)
-      
-      Spacer()
+    
+    let description: String
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack {
+                Spacer()
+                
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: 15, height: 15)
+                
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: 15, height: 15)
+                
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: 15, height: 15)
+                
+                Spacer()
+            }
+            .frame(height: 90)
+            .background(Color.gray)
+            
+            Rectangle().fill(Color.gray).frame(width: 50, height: 50)
+            
+            if let videoUrl = Bundle.main.url(forResource: "01", withExtension: "mov") {
+                VideoPlayer(player: AVPlayer(url: videoUrl))
+            }
+            
+            Text(self.description).foregroundColor(Theme.Palette.Text.Primary)
+            Spacer()
+        }
     }
-  }
 }
 
 struct OnboardingPageView_Previews: PreviewProvider {
-  static var previews: some View {
-    ObItemView(description: "Hello World")
-  }
+    static var previews: some View {
+        ObItemView(description: "Hello World")
+    }
 }
